af00cc042dc793d2c4ca60144b20c781c435baf5
Nam Ngô Văn <ngonam20081993@gmail.com>
2022-07-07 22:11:18 +0700

    Create project
    Khởi tạo dự án swiftUI

---
+++ LearnSwiftUI/LearnSwiftUI/LearnSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnSwiftUIApp.swift
+//  LearnSwiftUI
+//
+//  Created by Ngo Van Nam on 07/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearnSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

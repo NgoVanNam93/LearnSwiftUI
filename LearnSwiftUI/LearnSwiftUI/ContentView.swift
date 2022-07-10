61e3e76e0d49b2efe5fbc3a8995abc6f8549a5b8
Nam Ngô Văn <ngonam20081993@gmail.com>
2022-07-10 22:27:21 +0700

    Understanding and binding

--- LearnSwiftUI/LearnSwiftUI/ContentView.swift
+++ LearnSwiftUI/LearnSwiftUI/ContentView.swift
@@ -8,9 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var counter1 = 0
+    @State private var counter2 = 0
+    @State private var counter3 = 0
+    @State private var counterSum = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("\(counterSum)")
+                .font(.system(size: 100))
+            .fontWeight(.bold)
+            HStack {
+                CounterButton(counter: $counter1, counterSum: $counterSum, color: .blue)
+                    .frame(width: 100, height: 100)
+                CounterButton(counter: $counter2, counterSum: $counterSum, color: .green)
+                    .frame(width: 100, height: 100)
+                CounterButton(counter: $counter3, counterSum: $counterSum, color: .red)
+                    .frame(width: 100, height: 100)
+            }
+        }
+        /*VStack {
+            CounterButton(counter: $counter, color: .blue)
+            CounterButton(counter: $counter, color: .green)
+            CounterButton(counter: $counter, color: .red)
+        }
+        */
     }
 }
 
@@ -19,3 +42,27 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct CounterButton: View{
+    
+    @Binding var counter: Int
+    @Binding var counterSum: Int
+    
+    var color: Color
+    
+    var body: some View{
+        Button(action: {
+            self.counter += 1
+            self.counterSum += 1
+        }){
+            Circle()
+                .foregroundColor(color)
+                .overlay(
+                    Text("\(counter)")
+                        .font(.system(size: 80, weight: .bold, design: .rounded))
+                        .foregroundColor(.white)
+                )
+        }
+    }
+}
+

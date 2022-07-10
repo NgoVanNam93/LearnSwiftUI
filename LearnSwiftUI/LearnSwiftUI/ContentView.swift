a973dd0223732e3ace275b8669668f464520d395
Nam Ngô Văn <ngonam20081993@gmail.com>
2022-07-10 21:20:28 +0700

    understanding scrollview

--- LearnSwiftUI/LearnSwiftUI/ContentView.swift
+++ LearnSwiftUI/LearnSwiftUI/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView{
+            VStack{
+                CardView(image: "swiftui-button", category: "SwiftUI", heading: "Drawing a Border with Rounded Corners", author: "written by Simon Ng")
+                CardView(image: "macos-programming", category: "macOS", heading: "Building a Sample Editting App", author: "Gabriel Theodoropoulos")
+                CardView(image: "flutter-app", category: "Flluter", heading: "Building a Complex Layout with Flutter", author: "Lawrence Tan")
+                CardView(image: "natural-language-api", category: "iOS", heading: "What's New in Natural Language API", author: "Sai Kambampati")
+            }
+        }
     }
 }
 
@@ -19,3 +25,43 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct CardView: View{
+    var image: String
+    var category: String
+    var heading: String
+    var author: String
+    
+    var body: some View{
+        VStack{
+            Image(image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+            HStack {
+                VStack (alignment: .leading){
+                    Text(category)
+                        .font(.headline)
+                        .foregroundColor(.secondary)
+                    Text(heading)
+                        .font(.title)
+                        .fontWeight(.black)
+                        .foregroundColor(.primary)
+                        .lineLimit(3)
+                    Text(author.uppercased())
+                        .font(.caption)
+                        .foregroundColor(.secondary)
+                }
+                Spacer()
+            }
+            .padding()
+        }
+        .cornerRadius(10)
+        .overlay(
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(Color(.sRGB, red: 150/255, green: 150/255, blue: 150/255, opacity: 0.1), lineWidth: 1)
+        )
+        .padding([.top, .horizontal])
+    }
+}
+
